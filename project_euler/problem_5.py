81151d2cc34625e64396168c7417361e6aae4250
Alex Puga <alpuga8@gmail.com>
2019-03-09 00:57:28 -0500

    issue with problem 5

---
+++ project_euler/problem_5.py
@@ -0,0 +1,31 @@
+'''
+2520 is the smallest number that can be divided by each of the numbers from
+1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to n?
+'''
+
+
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+
+def smallest_mult(n):
+    least_cm = 1
+    i = 2
+    for i in range(n):
+        least_cm = lcm(least_cm, i)
+
+    print(least_cm)
+
+
+smallest_mult(10)
